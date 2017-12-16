4500e5b2601601a8c6ccdaaad74123e9926b56e3
José Ventura <st.never@gmail.com>
2017-12-16 11:06:47 -0200

    adjust relative path for images

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]',
-          publicPath: './dist/'
+          publicPath: process.env.NODE_ENV === 'production' ? './martian-chess/dist/' : './dist/'
         }
       }
     ]
